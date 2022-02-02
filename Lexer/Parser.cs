087018091f98d65c5539b91cbf9a2f704c822e1f
Burfedd <pavels.pranskus@gmail.com>
2022-02-02 11:40:20 +0200

    - Added Parser implementation

--- Lexer/Parser.cs
+++ Lexer/Parser.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lexer
 {
@@ -14,6 +10,60 @@ namespace Lexer
             _tokenizer = tokenizer;
         }
 
+        public Node ParseExpression()
+        {
+            Node exp = ParseAddSubtract();
 
+            if (_tokenizer.Token != Token.EOF)
+            {
+                throw new Exception("Invalid characters at end of expression");
+            }
+
+            return exp;
+        }
+
+        private Node ParseAddSubtract()
+        {
+            Node left = ParseLeaf();
+
+            while (true)
+            {
+                Func<double, double, double> op = null;
+
+                if (_tokenizer.Token == Token.Add)
+                {
+                    op = (a, b) => a + b;
+                } 
+                else if (_tokenizer.Token == Token.Subtract)
+                {
+                    op = (a, b) => a - b;
+                }
+
+                // If token isn't a binary operator
+                if (op == null)
+                {
+                    return left;
+                }
+
+                // If token is a binary operator, parse the other side
+                _tokenizer.NextToken();
+
+                Node right = ParseLeaf();
+                left = new NodeBinary(left, right, op);
+            }
+        }
+
+        private Node ParseLeaf()
+        {
+            if (_tokenizer.Token == Token.Number)
+            {
+                NodeNumber node = new NodeNumber(_tokenizer.Number);
+                _tokenizer.NextToken();
+                return node;
+            }
+
+            // Invalid token
+            throw new Exception($"Invalid token: {_tokenizer.Token}");
+        }
     }
 }
