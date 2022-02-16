b4aea9a0cb9ec64a355a7a5fb4eeac59fabbd1a2
Burfedd <leonquad2@gmail.com>
2022-02-16 19:55:22 +0200

    - Added simple Tokenizer test

---
+++ Lexer.Testing/TokenizerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using System.IO;
+
+namespace Lexer.Testing
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void GivenInputString_WhenNextToken_TokenChanges()
+        {
+            // Arrange
+            string input = "1+1";
+            Token expected = Token.Add;
+            Tokenizer t = new Tokenizer(new StringReader(input));
+
+            // Act
+            t.NextToken();
+            Token actual = t.Token;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
